2a2f52ff9da6dc432b2c88cb66ac6d309e1099bf
Jona Tadashi <jona@keemail.me>
2021-02-27 12:16:53 +0100

    Revert "remove "more videos""
    This reverts commit bed0ee9794f6293fbc1d94e13bc5f000074d4026.

--- lib/src/helpers/player_fragments.dart
+++ lib/src/helpers/player_fragments.dart
@@ -33,17 +33,10 @@ String youtubeIFrameTag(YoutubePlayerController controller) {
     'embed/${controller.initialVideoId}',
     params,
   );
-  return """
-
-    <div class="highPlayer">
-        <iframe id="player" type="text/html
-            style=""
-            src="$sourceUri"
-            frameborder="0" allowfullscreen
-        ></iframe>
-    </div>
-
-  """;
+  return '<iframe id="player" type="text/html"'
+      ' style="position:absolute; top:0px; left:0px; bottom:0px; right:10px;'
+      ' width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"'
+      ' src="$sourceUri" frameborder="0" allowfullscreen></iframe>';
 }
 
 ///
@@ -52,7 +45,6 @@ function play() {
   player.playVideo();
   return '';
 }
-
 function pause() {
   player.pauseVideo();
   return '';
